6c5562d0441eb27e184c909df9071001ba74e2ac
don-k-jacob <donkjacob.business@gmail.com>
2023-01-23 12:52:08 +0530

    Initial Commit

---
+++ BQG Watch App/BQGApp.swift
@@ -0,0 +1,17 @@
+//
+//  BQGApp.swift
+//  BQG Watch App
+//
+//  Created by don kora jacob on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BQG_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
